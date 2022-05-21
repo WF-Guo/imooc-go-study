4da53b6713f84ab5bdc43e95760dd355d351acb0
gwf <wefriender@gmail.com>
2022-05-21 21:14:55 +0800

    pprof

--- container/nonRepeatingSubstr/nonRepeteatingSubstr.go
+++ container/nonRepeatingSubstr/nonRepeteatingSubstr.go
@@ -19,11 +19,16 @@ import "fmt"
 }*/
 
 func lengthOfNonRepeatingSubstr(s string) int {
-	lastOccurred := make(map[rune]int)
+
+	lastOccurred := make([]int, 0xffff) //65535
+	for i := range lastOccurred {
+		lastOccurred[i] = -1
+	}
+
 	maxLength := 0
 	start := 0
 	for i, ch := range []rune(s) {
-		if lastI, ok := lastOccurred[ch]; ok && lastI >= start {
+		if lastI := lastOccurred[ch]; lastI != -1 && lastI >= start {
 			start = lastI + 1
 		}
 		if i-start+1 > maxLength {
